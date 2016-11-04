4eb21cf5124d1d151374b738efb37ac2e8d1c8c8
Anton Kostiuchkov <Anton.Kostiuchkov@teleste.com>
2016-11-04 18:26:13 +0100

    deal with it

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import babel from 'rollup-plugin-babel'
+import eslint from 'rollup-plugin-eslint'
+
+export default {
+    entry: 'src/scripts/main.js',
+    dest: 'build/js/main.min.js',
+    format: 'iife',
+    sourceMap: 'inline',
+    plugins: [
+        eslint({
+            exclude: [
+                'src/styles/**'
+            ]
+        }),
+        babel({
+            exclude: 'node_modules/**'
+        })
+    ]
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+import hello from './modules/mod1'
+
+hello()
